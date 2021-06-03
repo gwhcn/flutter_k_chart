fe30af49560f9e8678fa6de120f19e4467ee771c
Dat <imlegend103@gmail.com>
2021-06-03 16:23:31 +0700

    feat: migrate to null safety

--- lib/generated/intl/messages_zh.dart
+++ lib/generated/intl/messages_zh.dart
@@ -7,7 +7,7 @@
 // ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
-// ignore_for_file:unused_import, file_names
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
 
 import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
@@ -20,14 +20,15 @@ class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'zh';
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static _notInlinedMessages(_) => <String, Function> {
-    "change" : MessageLookupByLibrary.simpleMessage("涨跌额"),
-    "change_" : MessageLookupByLibrary.simpleMessage("涨跌幅"),
-    "close" : MessageLookupByLibrary.simpleMessage("收"),
-    "date" : MessageLookupByLibrary.simpleMessage("时间"),
-    "executed" : MessageLookupByLibrary.simpleMessage("成交量"),
-    "high" : MessageLookupByLibrary.simpleMessage("高"),
-    "low" : MessageLookupByLibrary.simpleMessage("低"),
-    "open" : MessageLookupByLibrary.simpleMessage("开")
-  };
+
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "change": MessageLookupByLibrary.simpleMessage("涨跌额"),
+        "change_": MessageLookupByLibrary.simpleMessage("涨跌幅"),
+        "close": MessageLookupByLibrary.simpleMessage("收"),
+        "date": MessageLookupByLibrary.simpleMessage("时间"),
+        "executed": MessageLookupByLibrary.simpleMessage("成交量"),
+        "high": MessageLookupByLibrary.simpleMessage("高"),
+        "low": MessageLookupByLibrary.simpleMessage("低"),
+        "open": MessageLookupByLibrary.simpleMessage("开")
+      };
 }
